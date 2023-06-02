780db418553fd6441223c5ba86922e30c0be9ade
Alexander von Renteln <alexander.renteln@gmail.com>
2023-06-02 11:59:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+console.log("SOME DEBUG OUTPUT! (in all caps -_-)" );
+
 const api = new window.CbWidgetApi.WidgetApi(window, 'widget-id', '*');
 
 api.authenticate()
     .then(response => response.token)
-    .then(token => fetch('http://localhost:8080/cb/api/v3/projects',{
+    .then(token => fetch('https://advantest.codebeamer.com/api/v3/projects',{
         headers: {
             authorization: 'Bearer ' + token
         }
