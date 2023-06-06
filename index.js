d9f3620c0ed858a342870292e22c788613e8e7bc
Martin Krombholz <martin.krombholz@avono.de>
2023-06-06 15:58:31 +0200

    fix date format

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function addRowToTable(item) {
 }
 
 function createTableCellWithDate(newRow, text) {
-    const actualText = text ? new Date(text).toLocaleDateString("en-US") : '-';
+    const actualText = text ? new Date(text).toString() : '-';
     const titleCell = newRow.insertCell();
     const titleText = document.createTextNode(actualText);
     titleCell.appendChild(titleText);
